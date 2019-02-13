009f8410c1ef7b792d3c03d08c49eb26569e4618
Yaroslav Sviridov <ys9617890962@gmail.com>
2019-02-13 21:47:00 +0300

    Add class for constants

---
+++ app/src/main/java/com/example/picturewatcher/Constants.java
@@ -0,0 +1,7 @@
+package com.example.picturewatcher;
+
+public final class Constants {
+    public static Integer ITEMS_PER_PAGE = 20;
+
+    public static Integer MEDIUM_IMAGE_W = 400, MEDIUM_IMAGE_H = 200;
+}
